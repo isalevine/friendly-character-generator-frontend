6844b32a136bca8896b475f3a75c5f336a14bec5
isalevine <isaacdlevine@gmail.com>
2019-04-29 21:05:26 -0700

    potential language to reuse SearchPreferenceOutput component, commented out about class code

--- src/components/forms/SearchPreferenceOutput.js
+++ src/components/forms/SearchPreferenceOutput.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react'
 
 
+// possible text to reuse/implement this component:
+// "Hmm, I'm thinking a character with..."
+// "...a playstyle that's [physical]..."
+// "...actions associated with '[tank]'..."
+// "...stats that prioritize [stamina]..."
+// "...and powers related to '[heal]'."
+
 class SearchPreferenceOutput extends Component {
 
   render() {
@@ -10,7 +17,7 @@ class SearchPreferenceOutput extends Component {
         <h4>SearchPreference Output:</h4>
           playstyle_preference: {this.props.formSearchPreference.playstyle_preference}<br />
           action_preference: {this.props.formSearchPreference.action_preference}<br />
-        stat_preference: {this.props.formSearchPreference.stat_preference}<br />
+          stat_preference: {this.props.formSearchPreference.stat_preference}<br />
           power_preference: {this.props.formSearchPreference.power_preference}
 
       </div>

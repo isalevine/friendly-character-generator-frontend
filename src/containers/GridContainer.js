12f0cd0b9b90f356607b3d320ce6a940392a85ee
isalevine <isaacdlevine@gmail.com>
2019-04-30 12:24:13 -0700

    REMOVED CSS GRID--all positions are now in pixels; GridContainer now deprecated; animation to clear cards (but not draw them from deck initially) now implemented

--- src/containers/GridContainer.js
+++ src/containers/GridContainer.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import FormContainer from './FormContainer'
 
 
+// DEPRECATED - all positions now using pixels
+
 class GridContainer extends Component {
 
   render() {
